0faa8bcda0f38defdec0301c96001e72b95715fa
ZakChrom <88797993+ZakChrom@users.noreply.github.com>
2025-04-21 21:53:58 +0300

    whoami

---
+++ coreutils/whoami.lua
@@ -0,0 +1,31 @@
+local function write(fd, data)
+    local err = syscall("write", fd, data)
+    return err == nil, err
+end
+
+local function exit(status)
+    -- WILL NEVER RETURN IF IT WORKED
+    local err = syscall("exit", status)
+    return err == nil, err
+end
+
+local function uinfo(user)
+    local err, x = syscall("uinfo", user)
+    return x, err
+end
+
+local function pinfo(pid)
+    local err, info = syscall("pinfo", pid)
+    return info, err
+end
+
+local function main(_)
+    write(0, assert(uinfo(assert(pinfo()).uid)).name .. "\n");
+end
+
+local ok, err = xpcall(main, debug.traceback, arg);
+if not ok then
+    write(2, err .. "\n");
+    assert(exit(1));
+end
+assert(exit(err or 0));
