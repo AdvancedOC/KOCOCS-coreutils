af76c50a8329506ec86b3bdb5c2538ec0e9e5331
ZakChrom <88797993+ZakChrom@users.noreply.github.com>
2025-04-22 01:04:20 +0300

    fs related things

---
+++ coreutils/format.lua
@@ -0,0 +1,30 @@
+local function write(fd, data)
+    local err = syscall("write", fd, data)
+    return err == nil, err
+end
+
+local function exit(status)
+    -- WILL NEVER RETURN IF IT WORKED
+    local err = syscall("exit", status)
+    return err == nil, err
+end
+
+local function main(argv)
+    if #argv == 0 then
+        write(0, "requires atleast 1 argument\n");
+        return 1;
+    end
+
+    local partUUID = assert(argv[1], "missing partition");
+    local fs = argv[2] or "okffs";
+
+    local part = assert(_K.fs.partitionFromUuid(partUUID), "partition not found");
+    assert(_K.fs.format(part, fs));
+end
+
+local ok, err = xpcall(main, debug.traceback, arg);
+if not ok then
+    write(2, err .. "\n");
+    assert(exit(1));
+end
+assert(exit(err or 0));
