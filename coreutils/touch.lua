5120574d2b8500477eb44cfa24bf500154c272d3
ZakChrom <88797993+ZakChrom@users.noreply.github.com>
2025-04-21 21:14:35 +0300

    touch

---
+++ coreutils/touch.lua
@@ -0,0 +1,29 @@
+local function write(fd, data)
+    local err = syscall("write", fd, data)
+    return err == nil, err
+end
+
+local function exit(status)
+    -- WILL NEVER RETURN IF IT WORKED
+    local err = syscall("exit", status)
+    return err == nil, err
+end
+
+local function touch(path, perms)
+    -- WILL NEVER RETURN IF IT WORKED
+    local err = syscall("touch", path, perms)
+    return err == nil, err
+end
+
+local function main(argv)
+    for i = 1, #argv do
+        touch(argv[i], (2^16)-1);
+    end
+end
+
+local ok, err = xpcall(main, debug.traceback, arg);
+if not ok then
+    write(2, err .. "\n");
+    assert(exit(1));
+end
+assert(exit(err or 0));
