3eb1dc0e31c5cb0797e5dc7b19ba4a6c5f865fa6
ZakChrom <88797993+ZakChrom@users.noreply.github.com>
2025-04-21 23:36:18 +0300

    sleep

---
+++ coreutils/sleep.lua
@@ -0,0 +1,25 @@
+local function write(fd, data)
+    local err = syscall("write", fd, data)
+    return err == nil, err
+end
+
+local function exit(status)
+    -- WILL NEVER RETURN IF IT WORKED
+    local err = syscall("exit", status)
+    return err == nil, err
+end
+
+local function main(argv)
+    if #argv == 0 then
+        write(0, "sleep: missing operand\n");
+        return 1;
+    end
+    coroutine.yield(assert(tonumber(argv[1])));
+end
+
+local ok, err = xpcall(main, debug.traceback, arg);
+if not ok then
+    write(2, err .. "\n");
+    assert(exit(1));
+end
+assert(exit(err or 0));
