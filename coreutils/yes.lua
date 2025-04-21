1b13861ddf8e7750d07dcdfb7b88423881d3c2f9
ZakChrom <88797993+ZakChrom@users.noreply.github.com>
2025-04-21 23:36:24 +0300

    yes

---
+++ coreutils/yes.lua
@@ -0,0 +1,30 @@
+local function write(fd, data)
+    local err = syscall("write", fd, data)
+    return err == nil, err
+end
+
+local function exit(status)
+    -- WILL NEVER RETURN IF IT WORKED
+    local err = syscall("exit", status)
+    return err == nil, err
+end
+
+local function main(argv)
+    local stuff;
+    if #argv == 0 then
+        stuff = "y";
+    else
+        stuff = table.concat(argv, " ");
+    end
+    while true do
+        write(0, stuff .. "\n");
+        coroutine.yield();
+    end
+end
+
+local ok, err = xpcall(main, debug.traceback, arg);
+if not ok then
+    write(2, err .. "\n");
+    assert(exit(1));
+end
+assert(exit(err or 0));
