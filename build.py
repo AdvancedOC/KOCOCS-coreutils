d127a7b34dbfcbb243370e1ea45bcb59770c3c5d
ZakChrom <88797993+ZakChrom@users.noreply.github.com>
2025-04-23 17:38:46 +0300

    fix build system

--- build.py
+++ build.py
@@ -1,36 +1,47 @@
-import os, shutil, subprocess
-with open("/etc/hostname", "r") as f:
-    if f.read() != "calion-Computer\n":
-        print("TODO: Make the build system work for others")
-        exit(1)
+import os, shutil, sys
+#os.chdir("../")
+#if os.path.isdir("./rootfs/build"): shutil.rmtree("./rootfs/build")
+#os.mkdir("./rootfs/build")
 
-os.chdir("../")
-if os.path.isdir("./rootfs/build"): shutil.rmtree("./rootfs/build")
-os.mkdir("./rootfs/build")
+if not os.path.isfile("./coreutils.kpm"):
+    print("Run in coreutils directory")
+    exit(1)
+if len(sys.argv) != 3:
+    print("Give tools and lib path as arg")
+    exit(1)
+
+tools = sys.argv[1];
+assert(tools != "")
+libs = sys.argv[1];
+assert(libs != "")
+os.environ["LUA_PATH"] = libs + "/../?.lua"
+
+if os.path.isdir("./build"): shutil.rmtree("./build")
+os.mkdir("./build")
 
 def build(package):
-    for f in os.listdir(f"./rootfs/{package}"):
+    for f in os.listdir(f"./{package}"):
         if f.endswith(".lua"):
             name = f.removesuffix(".lua")
-            os.system(f"lua ./tools/luac.lua -o ./rootfs/build/{name}.o -m main ./rootfs/{package}/{f}")
-            os.system(f"lua ./tools/ld.lua -o ./rootfs/build/{name} ./rootfs/build/{name}.o -l/lib/liblua.so" + (" -l/lib/libkelp.so" if name == "klc" else ""))
+            assert(os.system(f"lua {tools}/luac.lua -o ./build/{name}.o -m main ./{package}/{f}") == 0)
+            assert(os.system(f"lua {tools}/ld.lua -o ./build/{name} ./build/{name}.o -l{libs}/liblua.so" + (f" -l{libs}/libkelp.so" if name == "klc" else "")) == 0)
 build("coreutils")
 build("util")
 build("extra")
 
-def build_rootfs():
-    if os.path.isdir("./bin"): shutil.rmtree("./bin")
-    os.mkdir("./bin")
+# def build_rootfs():
+#     if os.path.isdir("./bin"): shutil.rmtree("./bin")
+#     os.mkdir("./bin")
 
-    if not os.path.isdir("./mnt"): os.mkdir("./mnt")
-    if not os.path.isdir("./tmp"): os.mkdir("./tmp")
-    if not os.path.isdir("./dev"): os.mkdir("./dev")
+#     if not os.path.isdir("./mnt"): os.mkdir("./mnt")
+#     if not os.path.isdir("./tmp"): os.mkdir("./tmp")
+#     if not os.path.isdir("./dev"): os.mkdir("./dev")
 
-    for f in os.listdir(f"./rootfs/build/"):
-        if not f.endswith(".o"):
-            os.system(f"cp ./rootfs/build/{f} ./bin/")
-    os.system("cp luart ./bin/lua")
-    os.system("cp basicTTY.lua ./bin/")
-    os.system("cp ./bin/kterm ./bin/sh")
+#     for f in os.listdir(f"./rootfs/build/"):
+#         if not f.endswith(".o"):
+#             os.system(f"cp ./rootfs/build/{f} ./bin/")
+#     os.system("cp luart ./bin/lua")
+#     os.system("cp basicTTY.lua ./bin/")
+#     os.system("cp ./bin/kterm ./bin/sh")
 
-build_rootfs()
+# build_rootfs()
